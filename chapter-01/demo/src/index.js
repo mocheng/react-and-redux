54bf6d496c03a6492031a05c3829964ee0cf6e5a
Morgan Cheng <morgan.chengmo@gmail.com>
2016-12-10 16:17:25 +0800

    1.2/add ClickCounter

--- chapter-01/demo/src/index.js
+++ chapter-01/demo/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+//import App from './App';
+import ClickCounter from './ClickCounter';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <ClickCounter/>,
   document.getElementById('root')
 );
