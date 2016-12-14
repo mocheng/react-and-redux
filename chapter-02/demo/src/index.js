d20f726d008ff93820b196bcce744931781c829b
Morgan Cheng <morgan.cheng@hulu.com>
2016-12-14 14:32:39 +0800

    fix error

---
+++ chapter-02/demo/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import App from './App';
+import ClickCounter from './ClickCounter';
+import './index.css';
+
+ReactDOM.render(
+  <ClickCounter/>,
+  document.getElementById('root')
+);
