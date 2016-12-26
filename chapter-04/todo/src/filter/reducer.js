b1e281153a0a6553c943aab30d8d57a70decff5a
Morgan Cheng <morgan.chengmo@gmail.com>
2016-12-26 23:07:39 +0800

    remove redundant code

--- chapter-04/todo/src/filter/reducer.js
+++ chapter-04/todo/src/filter/reducer.js
@@ -2,7 +2,6 @@ import {SET_FILTER} from './actionTypes.js';
 import {FilterTypes} from '../constants.js'
 
 export default (state = FilterTypes.ALL, action) => {
-  console.log('#action', action);
   switch(action.type) {
     case SET_FILTER: {
       return action.filter;
--- chapter-04/todo/src/todoList/actionTypes.js
+++ chapter-04/todo/src/todoList/actionTypes.js
@@ -1,2 +1,3 @@
 export const ADD_TODO = 'TODO/ADD';
-export const TOGGLE_TODO =  'TODO/TOGGLE';
+export const TOGGLE_TODO = 'TODO/TOGGLE';
+
