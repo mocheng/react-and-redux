91144c4f8240698c4800337ec192187d6c63b882
Morgan Cheng <morgan.chengmo@gmail.com>
2016-12-28 22:50:08 +0800

    add Perf

--- chapter-04/todo/src/Store.js
+++ chapter-04/todo/src/Store.js
@@ -3,6 +3,11 @@ import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
 import {reducer as todoReducer} from './todos';
 import {reducer as filterReducer} from './filter';
 
+import Perf from 'react-addons-perf'
+
+const win = window;
+win.Perf = Perf
+
 const reducer = combineReducers({
   todos: todoReducer,
   filter: filterReducer
@@ -13,7 +18,6 @@ if (process.env.NODE_ENV !== 'production') {
   middlewares.push(require('redux-immutable-state-invariant')());
 }
 
-const win = window;
 const storeEnhancers = compose(
   applyMiddleware(...middlewares),
   (win && win.devToolsExtension) ? win.devToolsExtension() : (f) => f,
