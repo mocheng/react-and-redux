8b5ae1a6de514c046b32aade5f981f8ff7306861
Morgan Cheng <morgan.cheng@hulu.com>
2016-12-29 09:44:48 +0800

    copy todo to chapter-05

---
+++ chapter-05/todo_perf/src/Store.js
@@ -0,0 +1,26 @@
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
+
+import {reducer as todoReducer} from './todos';
+import {reducer as filterReducer} from './filter';
+
+import Perf from 'react-addons-perf'
+
+const win = window;
+win.Perf = Perf
+
+const reducer = combineReducers({
+  todos: todoReducer,
+  filter: filterReducer
+});
+
+const middlewares = [];
+if (process.env.NODE_ENV !== 'production') {
+  middlewares.push(require('redux-immutable-state-invariant')());
+}
+
+const storeEnhancers = compose(
+  applyMiddleware(...middlewares),
+  (win && win.devToolsExtension) ? win.devToolsExtension() : (f) => f,
+);
+
+export default createStore(reducer, {}, storeEnhancers);
---
+++ chapter-05/todo_perf/src/todos/views/todos.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AddTodo from './addTodo.js';
+import TodoList from './todoList.js';
+
+export default () => {
+  return (
+    <div>
+      <AddTodo />
+      <TodoList />
+    </div>
+  );
+}
+
