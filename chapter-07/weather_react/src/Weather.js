d527fedf2a57ac41e8f41c928d4d0a0d703d2d12
Morgan Cheng <morgan.cheng@hulu.com>
2017-01-01 11:13:09 +0800

    init weather_react

---
+++ chapter-07/weather_react/src/Weather.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const cityCode = 101010100;
+
+class Weather extends React.Component {
+  constructor() {
+    super(...arguments);
+
+    this.state = {weather: null};
+  }
+
+  componentDidMount() {
+    const apiUrl = `/data/cityinfo/${cityCode}.html`;
+    fetch(apiUrl).then((response) => {
+      if (response.status !== 200) {
+        throw new Error('Fail to get response with status ' + response.status);
+      }
+
+      response.json().then((responseJson) => {
+        this.setState({weather: responseJson.weatherinfo});
+      }).catch((error) => {
+        this.setState({weather: null});
+      });
+    }).catch((error) => {
+      this.setState({weather: null});
+    });
+  }
+
+  render() {
+    if (!this.state.weather) {
+      return <div>暂无数据</div>;
+    }
+
+    const {city, weather, temp1, temp2} = this.state.weather;
+
+    return (
+      <div>
+        {city} {weather} 最低气温 {temp1} 最高气温 {temp2}
+      </div>
+    )
+  }
+}
+
+export default Weather;
---
+++ chapter-07/weather_react/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Weather from './Weather';
+import './index.css';
+
+ReactDOM.render(
+  <Weather />,
+  document.getElementById('root')
+);
