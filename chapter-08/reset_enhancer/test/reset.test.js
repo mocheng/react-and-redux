94f6ad43e4eba10f622900eae92ff6baf0bf0ea0
Morgan Cheng <morgan.cheng@hulu.com>
2017-01-03 14:07:45 +0800

    add unit test of reset_enhancer

--- chapter-08/reset_enhancer/test/reset.test.js
+++ chapter-08/reset_enhancer/test/reset.test.js
@@ -1,7 +1,34 @@
-import ResetEnhancer from '../src/reset.js';
+import resetEnhancer from '../src/reset.js';
+import {createStore} from 'redux';
 
 describe('reset enhancer', () => {
-  it('reset action updates store reducer and state', () => {
-    //TODO:
+  const configureStore = (reducer) => {
+    return createStore(reducer, resetEnhancer);
+  };
+
+  let store;
+
+  beforeEach(() => {
+    const reducer = (state, action) => ({payload: action.payload});
+    store = configureStore(reducer);
+  });
+
+  it('should not break store functionality', () => {
+    store.dispatch({type: 'any', payload: 'bar'});
+
+    expect(store.getState()).toEqual({payload: 'bar'});
+  });
+
+  it('reset', () => {
+    it('should reset state and reducer', () => {
+      const newReducer = (state, action) =>({newPayload: action.payload});
+      const newState = {newPayload: 'new'};
+
+      store.reset(newReducer, newState);
+      expect(store.getState()).toEqual(newState);
+
+      store.dispatch({type: 'any', payload: 'changed'});
+      expect(store.getState()).toEqual({newPayload: 'changed'});
+    });
   });
 });
