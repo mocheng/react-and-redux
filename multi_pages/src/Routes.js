89d875b80e0474f76482a98bd985acc08caa8c43
Morgan Cheng <morgan.cheng@hulu.com>
2017-01-09 10:39:30 +0800

    add IndexRoute for home page

--- multi_pages/src/Routes.js
+++ multi_pages/src/Routes.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import {Router, Route, browserHistory} from 'react-router';
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
 
 import App from './pages/App.js';
+import Home from './pages/Home.js';
 import About from './pages/About.js';
 import NotFound from './pages/NotFound.js';
 
 const Routes = () => (
   <Router history={browserHistory}>
     <Route path="/" component={App}>
+      <IndexRoute component={Home} />
       <Route path="about" component={About} />
       <Route path="*" component={NotFound} />
     </Route>
--- multi_pages/src/components/navigation/index.js
+++ multi_pages/src/components/navigation/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 
 import {Link} from 'react-router';
 
+const liStyle = {
+  display: 'inline-block',
+  margin: '10px 20px'
+}
+
 const view = () => {
   return (
     <div>
       <ul>
-        <li><Link to="/home">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
+        <li style={liStyle}><Link to="/home">Home</Link></li>
+        <li style={liStyle}><Link to="/about">About</Link></li>
       </ul>
     </div>
   );
