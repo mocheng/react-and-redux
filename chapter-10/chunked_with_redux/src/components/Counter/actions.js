e193fbaa0177cad13741b4acf28ede8a42320f6f
Morgan Cheng <morgan.cheng@hulu.com>
2017-01-12 15:00:02 +0800

    refactor Counter acitons.js

--- chapter-10/chunked_with_redux/src/components/Counter/actions.js
+++ chapter-10/chunked_with_redux/src/components/Counter/actions.js
@@ -1,16 +1,11 @@
 import * as ActionTypes from './actionTypes.js';
 
-export const increment = (counterCaption) => {
-  return {
-    type: ActionTypes.INCREMENT,
-    counterCaption: counterCaption
-  };
-};
+export const increment = (counterCaption) => ({
+  type: ActionTypes.INCREMENT,
+  counterCaption: counterCaption
+});
 
-export const decrement = (counterCaption) => {
-  return {
-    type: ActionTypes.DECREMENT,
-    counterCaption: counterCaption
-  };
-};
-
+export const decrement = (counterCaption) => ({
+  type: ActionTypes.DECREMENT,
+  counterCaption: counterCaption
+});
