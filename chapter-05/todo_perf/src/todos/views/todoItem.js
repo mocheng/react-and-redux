f574921cafa2b3b0fdc720d3d6098c6c418e2c02
Morgan Cheng <morgan.cheng@hulu.com>
2017-01-24 15:03:40 +0800

    remove redundant bindActionCreators

--- chapter-05/todo_perf/src/todos/views/todoItem.js
+++ chapter-05/todo_perf/src/todos/views/todoItem.js
@@ -1,5 +1,5 @@
 import React, {PropTypes} from 'react';
-import {bindActionCreators} from 'redux';
+//import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import {toggleTodo, removeTodo} from '../actions.js';
 
